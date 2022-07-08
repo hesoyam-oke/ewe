291fe2b96c21c809be852d36978bc68d9c3e7827
hesoyam-oke <mr.tucoy1@gmail.com>
2022-07-08 18:15:17 +0700

    o

---
+++ root.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+sed -i "/PasswordAuthentication no/d" /etc/ssh/sshd_config
+echo "" | sudo tee -a /etc/ssh/sshd_config
+echo "" | sudo tee -a /etc/ssh/sshd_config
+echo "PermitRootLogin yes" | sudo tee -a /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+systemctl restart sshd
+ip=$(curl ifconfig.me/ip)
+clear
+                                
+echo
+echo "Cara Mudah Untuk Root Dan Login Vps"
+echo "============================================"
+user=root
+echo "Masukkan Password: " 
+read -e pw
+pwe=$(mkpasswd $pw)
+
+clear
+usermod --password $pwe $user
+clear
+
+echo ""
+printf "Please Save This VPS Account Information
+============================================
+Ip address = $ip
+Username   = $user
+Password   = $pw
+============================================"
+sleep 1
+echo ""
+echo 
+exit
